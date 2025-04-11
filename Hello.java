364e25e290d4d0b0c3a2260387a0ff9ca8ee59ff
Woulfe1 <justawolfpup2@gmail.com>
2025-04-11 19:21:36 +0000

    created hello

--- Hello.java
+++ Hello.java
@@ -1,9 +1,19 @@
 public class Hello {
+    
+    
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        greet(getName());
 
     }
-    
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!" );
+    }
+    public static String getName() {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        return name;
+    }
 }
